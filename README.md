dac7f960e35a067a018826cef1cca351c2e9d32c
Edward Slipszenko <edward.slipszenko@myport.ac.uk>
2013-10-05 22:24:41 +0100

    Currently you can enter and exit the slide show, but exiting doens't return the content to its original state and there is no validation once you've reached the end of the slideshow.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 h5pres
 ======
 
-h5pres, HTML5 presentation JavaScript drop-in, entirely inspired by: https://github.com/ear1grey/h5pres
+h5pres, HTML5 presentation JavaScript drop-in, heavily based on and partially copied from: https://github.com/ear1grey/h5pres
+
+At the moment designed to have none of the styling and just single layer sections. Multi depth sections and auto index generation will come soon.
+
+This will only work in modern browsers and I currently have no intention of changing that
---
+++ src/h5pres.js
@@ -0,0 +1,106 @@
+// Create a namespace for the program, if not already created
+var szko = szko ? szko : {};
+
+szko.h5pres = (function () {
+    "use strict";
+
+    // Private vars
+    var presenting = false,
+        current = 1,
+        keys = {
+            left : 37,
+            up : 38,
+            right : 39,
+            down : 40,
+            fullStop : 190
+        },
+
+
+    setCurrentSlide = function (slide) {
+        current = slide;
+        // Hide everything at the first level (except the article tag), and all slides
+        var everything = document.querySelectorAll("body>*:not(article), article section");
+        for(var i = 0; i < everything.length; i++) {
+            everything[i].classList.add("szko-h5pres-hide");
+        }
+        // Show the slide
+        document.querySelector("article section:nth-child(" + current + ")").classList.remove("szko-h5pres-hide");
+    },
+
+
+    startPresenting = function () {
+        console.log("Starting slideshow.")
+        setCurrentSlide(current);
+        presenting = true;
+
+        // Go fullscreen        
+        var docElm = document.documentElement;
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        } else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        } else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        }
+    },
+
+
+    stopPresenting = function () {
+        console.log("Stopping slideshow.")
+        // Return to the normal view
+        presenting = false;
+
+        // Exit fullscreen      
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    },
+
+    
+    togglePresentation = function () {
+        if (!presenting) {
+            startPresenting();
+        } else {
+            stopPresenting();
+        }
+    },
+
+
+    keypress = function (e) {
+        switch (e.keyCode) {
+            case (keys.right):
+            case (keys.down):
+                setCurrentSlide(current + 1);
+                break;
+            case (keys.left):
+            case (keys.up):
+                setCurrentSlide(current - 1);
+                break;
+            case (keys.fullStop):
+                togglePresentation();
+                break;
+        }
+        return false;
+    },
+
+
+    init = function () {
+        console.log("h5pres initiated");
+        window.addEventListener('keydown', keypress.bind(szko.h5pres), false);
+    };
+
+
+    // Return exposes some private functions as public
+    return {
+        init : init.bind(szko.h5pres),
+        stopPresenting: stopPresenting.bind(szko.h5pres),
+        startPresenting: startPresenting.bind(szko.h5pres)
+    };
+
+}());
+
+szko.h5pres.init();
